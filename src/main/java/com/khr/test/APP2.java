37203976ab4c5a7364e5a8e851d6bddcb90ed240
konghuari <konghuari@dxy.cn>
2021-01-29 10:39:18 +0800

    [新增功能](多大的): 打点
    的

---
+++ src/main/java/com/khr/test/APP2.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2021, DXY All Rights Reserved.
+ * FileName: APP2
+ * Author:   konghuari
+ * Date:     2021/1/29 10:38
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.khr.test;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author konghuari
+ * @create 2021/1/29
+ * BUG
+ * @version 1.0.0
+ */
+public class APP2 {
+}
